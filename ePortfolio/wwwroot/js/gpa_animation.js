1d0d0b52e9fc0d358e4568b3eb44c33964c1bc2a
AsianJohnBoi <Jbssantias@hotmail.com>
2018-07-10 14:54:42 +1000

    changes to home page

--- ePortfolio/wwwroot/js/gpa_animation.js
+++ ePortfolio/wwwroot/js/gpa_animation.js
@@ -9,3 +9,17 @@ $('.count').each(function () {
 			}
 		});
 });
+
+function rerun() {
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+                duration: 2500,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(now.toFixed(3));
+                }
+            });
+    });
+}
